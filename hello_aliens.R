4ced73cb80ef7a119af31c1076449082c7f616ca
Andrew Wissinger <andrewfw@andrew.cmu.edu>
2017-12-10 20:29:14 -0500

    Message to Aliens

---
+++ hello_aliens.R
@@ -0,0 +1,10 @@
+cat("Hello extraterrestrail life in the star system TRAPPIST-1. \n
+We are homosapiens of the planet Earth, orbiting the Sun, in the Milkiway Galaxy 
+and we come in peace.  Based on our calculations, your star system is both able to have 
+liquid water and close enough to Earth's mass and radius to contain a stable atmosphere.  
+Our goal of humanity is to discover other intelligent life in the universe and to learn 
+from each other.  In doing so, we want to find new star systems to travel to and find new 
+settlements for humanity.  Earth has been our home where humanity lives, but we do not want
+it to be our only footprint in the universe.  Our question to you, intelligent life in the 
+system TRAPPIST-1, do you want us help provide us another home in the stars, and search for 
+other intelligent life in the universe?")
